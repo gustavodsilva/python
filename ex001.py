ad6c39f303bd8a859f75eeb34d7af45040e71e4f
GUSTAVO DA SILVA <109164339+gustavodsilva@users.noreply.github.com>
2022-07-12 15:44:26 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,17 @@
+nome = "Gustavo da Silva"
+idade = 23
+estudo = "Análise e Desenvolvimento de Sistemas"
+objetivo = "Primeira experiência na área de tecnologia."
+numero1 = 3
+numero2 = 5
+numero3 = 9
+
+print("Adoraria compartilhar que estou estudando Python, para desenvolvimento web!")
+
+numero1 + numero2  
+numero2 * numero3  
+
+print("Meu nome é", nome)
+print("tenho", idade, "anos")
+print("Estudante de", estudo)
+print("procurando me desenvolver e buscando minha", objetivo)
